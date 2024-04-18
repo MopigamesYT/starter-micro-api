dd3a68617b67f476637f90b3d7992e1968e21292
MopigamesYT <mopigames@proton.me>
2024-04-18 20:19:03 +0200

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const { Client } = require('discord.js-selfbot-v13')
+const client = new Client()
+
+client.on('ready', async () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+
+    const channel = await client.channels.fetch(process.env.BUMP_CHANNEL)
+    
+    async function bump() {
+        await channel.sendSlash('302050872383242240', 'bump')
+        console.count('Bumped!')
+    }
+
+    function loop() {
+        // send bump message every 2-3 hours, to prevent detection.
+        var randomNum = Math.round(Math.random() * (9000000 - 7200000 + 1)) + 7200000
+        setTimeout(function () {
+            bump()
+            loop()
+        }, randomNum)
+    }
+    
+    bump()
+    loop()
+})
+
+client.login(process.env.TOKEN)
